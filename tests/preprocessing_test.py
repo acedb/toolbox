60928c873d7debd28837ce7ea39d3e338c7c0024
Alice de Bokay <alicedebokay@googlemail.com>
2020-11-23 17:03:54 +0100

    Corrected tests

--- tests/preprocessing_test.py
+++ tests/preprocessing_test.py
@@ -3,9 +3,6 @@ from toolbox.preprocessing import NLPPreprocessor
 import pytest
 
 import string
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
 
 def test_punctuation_remover():
     # function that removes punctuation from a given text
@@ -24,22 +21,6 @@ def test_to_lowercase():
     # function that makes all text lowercase
     text = 'Did you Know This'
     assert text.lower() == 'did you know this'
-
-def test_stopword_remover():
-    # removes stopwords from given text and returns text as list of words
-    text = 'i am so very happy today'
-
-    # english stopwords stored in variable stop_words
-    stop_words = set(stopwords.words('english'))
-
-    # splits text into list of words
-    word_tokens = word_tokenize(text)
-
-    # removes stopwords from text
-    text_list = [word for word in word_tokens if not word in stop_words]
-
-    # returns list of non-stopwords
-    assert text_list == ['happy', 'today']
 
 def test_list_concat():
     # converts list of words back into string
